3cdf96c36f196c2c2cdee4fee49b141a25adf877
Martin Grossmann <mhgrossmann@yahoo.com>
2016-04-11 15:37:42 -0500

    styling filterbox

--- app/scripts/components/filterbox.js
+++ app/scripts/components/filterbox.js
@@ -4,15 +4,15 @@ const FilterBox = React.createClass({
 	render: function() {
 		return (
 			<div className='filterbox'>
-			<div className='keywords'>
-				<h1>Keywords</h1>
-				<input type='text'/>
-			</div>
-			<div className='location'>
-				<h1>Location</h1>
-				<input type='text'/>
-			</div>
-				<input type="button"/>
+				<div className='left'>
+					<h1>Keywords</h1>
+					<input type='text' className='keywords'/>
+				</div>
+				<div className='right'>
+					<h1>Location</h1>
+					<input type='text' className='location'/>
+					<button type='button' className='search'>Search</button>
+				</div>
 			</div>
 			);
 	}
