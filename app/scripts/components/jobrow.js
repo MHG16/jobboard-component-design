88a52ed1f1d98f54bd9babb82754546e3221b295
Martin Grossmann <mhgrossmann@yahoo.com>
2016-04-11 17:19:31 -0500

    added bullet

--- app/scripts/components/jobrow.js
+++ app/scripts/components/jobrow.js
@@ -6,22 +6,22 @@ const JobRow = React.createClass({
 			<div>
 				<h1 class="">Senior Frontend Engineer</h1>
 				<h2 class="">NSONE</h2>
+				<span class="bullet">&#8226;</span>
 				<h3 class="">New York, NY</h3>
 				<p>Senior Frontend Engineer.  Solve Hard Problems With a Team of Infrastructure Pros.  NSONE is based in New York...
 				</p>
-				
-
-
+				<section class="keywords">
+					<p>angularjs</p>
+					<p>d3.js</p>
+					<p>javascript</p>
+					<p>ui</p>
+					<p>css</p>
+				</section>
 			</div>   
 			);
 	}
 
 });
-
-
-
-
-
 
 
 
