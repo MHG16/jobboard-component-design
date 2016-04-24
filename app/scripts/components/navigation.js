ae1e1357a8874c56dd44c7fda5aa9da27eb8855c
Martin Grossmann <mhgrossmann@yahoo.com>
2016-04-24 18:14:20 -0500

    working on styling filterbox.js

--- app/scripts/components/navigation.js
+++ app/scripts/components/navigation.js
@@ -4,15 +4,16 @@ import logo from './../../images/logo.png';
 const Navigation = React.createClass({
 	render: function() {
 		return (
-			<div className="nav">
-			<img src ={logo}/>
-			<a href = '#' className="company">Fresh Jobs</a>
-			<a href = '#' className="links">JOBS</a>
-			<a href = '#' className="links">COMPANIES</a>
-			<a href = '#' className="links">CITIES</a>
-			<a href = '#' className="links">WHY FRESH?</a>
-			<a href = '#' className="links">FOR EMPLOYERS</a>
-			</div>
+				<div className="nav">
+				<img src ={logo}/>
+					<a href = '#' className="company">Fresh Jobs</a>
+					<a href = '#' className="links">JOBS</a>
+					<a href = '#' className="links">COMPANIES</a>
+					<a href = '#' className="links">CITIES</a>
+					<a href = '#' className="links">WHY FRESH?</a>
+					<a href = '#' className="links">FOR EMPLOYERS</a>
+				</div>
+
 			);
 	}
 
